ccc3f23927398c9e158709698e071d883055aadf
Nisha Kandpal <nishakandpal7557@gmail.com>
2024-08-18 10:33:32 +0530

    Food delivery application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,16 @@ import Home from './pages/Home/home';
 import Cart from './pages/Cart/cart'; 
 import PlaceOrder from './pages/PlaceOrder/placeorder';
 import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopups/LoginPopup';
+import { useState } from 'react';
 
 const App = () => {
+  const [showLogin,setShowLogin] = useState(false)
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/> :<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin = {setShowLogin}/>
       <Routes>
         <Route path = '/' element={<Home/>} />
         <Route path = '/cart' element={<Cart/>} />
